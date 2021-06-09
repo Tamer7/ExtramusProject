f9fb77236bb06f93a457fd0b1d342e32aa53da39
Radek <43780738+RadekHM@users.noreply.github.com>
2021-06-09 11:41:32 +0200

    fixed exporting to excel

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -9,33 +9,50 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 class UsersExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
 
-        return Booking::select('id','place_id', 'payer_name', 'user_fullname', 'user_email', 'user_phone', 'user_no_of_guest', 'user_no_of_babies', 'user_checkin', 'user_checkout', 'is_approved', 'user_promo', 'user_payment_type', 'user_booking_tracking_id', 'created_at','paid_ammount')->get();
+        return Booking::select(
+            'id',
+            'place_id',
+            'payer_name',
+            'user_fullname',
+            'user_email',
+            'user_phone',
+            'user_no_of_guest',
+            'user_no_of_babies',
+            'user_checkin',
+            'user_checkout',
+            'is_approved',
+            'user_promo',
+            'user_payment_type',
+            'user_booking_tracking_id',
+            Booking::raw('(created_at +  INTERVAL 2 HOUR) AS created_at'),
+            'paid_ammount'
+        )->get();
     }
 
     public function headings(): array
     {
         return [
-          'ID',
-          'Place ID',
-          'Payer Name',
-          'Full Name',
-          'Email',
-          'Phone',
-          'Number of adults',
-          'Number of babies',
-          'Arrival Time',
-          'Checkout Time',
-          'Approval',
-          'Promo',
-          'Payment Type',
-          'Tracking Id',
-          'Created At',
-          'Total Ammount',
+            'ID',
+            'Place ID',
+            'Payer Name',
+            'Full Name',
+            'Email',
+            'Phone',
+            'Number of adults',
+            'Number of babies',
+            'Arrival Time',
+            'Checkout Time',
+            'Approval',
+            'Promo',
+            'Payment Type',
+            'Tracking Id',
+            'Created At',
+            'Total Ammount',
         ];
     }
 }
